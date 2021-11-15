c5c3d988810b984d3e1f2f681766258f1adade65
Sam Bonill <sam@MacBook-Pro-de-Sam-2.local>
2021-11-15 17:51:20 -0500

    update version

--- greetings.go
+++ greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hello %v, Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
